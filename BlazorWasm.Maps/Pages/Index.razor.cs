e6e32ca5b79e81efacfeb7a60381ac8199377d37
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-10-30 21:19:51 +0630

    [Added] JsRuntime

---
+++ BlazorWasm.Maps/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿
+using BlazorWasm.Maps.Models;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+
+namespace BlazorWasm.Maps.Pages
+{
+    public partial class Index
+    {
+        private List<BaganMapInfoModel> _baganMapInfo;
+        private List<BaganMapInfoDetailModel> _baganMapInfoDetail;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await LoadMap();
+            }
+        }
+
+        private async Task LoadMap()
+        {
+            _baganMapInfo = _mapService.BaganMapInfo;
+            _baganMapInfoDetail = _mapService.BaganMapInfoDetail;
+            await _jsRuntime.InvokeVoidAsync("loadMap", JsonConvert.SerializeObject(_baganMapInfo));
+        }
+    }
+}
